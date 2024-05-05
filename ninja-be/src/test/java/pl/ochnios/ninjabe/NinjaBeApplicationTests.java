de45fdf1b744634458ff21c7b4a90818e4cd2b7c
Szymon Ochnio <szymonochnio15@gmail.com>
2024-05-05 22:35:58 +0200

    DN-2 Initialize backend with Spring initializr

---
+++ ninja-be/src/test/java/pl/ochnios/ninjabe/NinjaBeApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.ochnios.ninjabe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NinjaBeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
